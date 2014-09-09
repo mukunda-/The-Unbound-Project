04690273eb61758e5c394e22f0d2e58349cc65a5
mukunda- <mukunda@mukunda.com>
2014-09-09 01:36:44 -0600

    getting ready to test some entities

---
+++ dev/source/game/testentity.h
@@ -0,0 +1,18 @@
+//============================  The Unbound Project  ==========================//
+//                                                                             //
+//========== Copyright © 2014, Mukunda Johnson, All rights reserved. ==========//
+
+#pragma once
+
+#include "game/entity.h"
+
+namespace Game {
+
+class TestEntity : public Entity { 
+	int data;
+
+public:
+
+};
+
+}
