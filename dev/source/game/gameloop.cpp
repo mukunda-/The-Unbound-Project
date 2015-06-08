2e44adaaf42f383f4d720ab100869cf6bf84cb1c
mukunda- <mukunda@mukunda.com>
2015-06-08 10:25:08 -0600

    a

--- dev/source/game/gameloop.cpp
+++ dev/source/game/gameloop.cpp
@@ -3,3 +3,13 @@
 //========== Copyright © 2015, Mukunda Johnson, All rights reserved. ==========//
 
 #include "stdafx.h"
+
+//-------------------------------------------------------------------------------
+namespace Game {
+
+//-------------------------------------------------------------------------------
+GameLoop::GameLoop( double refresh_rate ) {
+
+}
+
+}
