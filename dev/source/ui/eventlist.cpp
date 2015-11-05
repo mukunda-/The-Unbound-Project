18ece44ce182f466bbad168f9884158daf570e7f
mukunda- <mukunda@mukunda.com>
2015-11-05 15:44:26 -0600

    a

--- dev/source/ui/eventlist.cpp
+++
@@ -1,9 +0,0 @@
-//==========================  The Unbound Project  ==========================//
-//                                                                           //
-//========= Copyright © 2015, Mukunda Johnson, All rights reserved. =========//
-
-#include "stdafx.h"
-
-namespace Ui {
-
-}
