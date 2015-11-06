0971ca1e0047850143ebf7427c91a4de889ef650
mukunda- <mukunda@mukunda.com>
2015-11-06 12:30:05 -0600

    a

--- dev/source/util/argstring.h
+++ dev/source/util/argstring.h
@@ -10,10 +10,10 @@
 //-----------------------------------------------------------------------------
 namespace Util {
 
-/// ---------------------------------------------------------------------------
-/// A class to parse a command string. This must be treated as a temporary
-/// object and should not be stored in an object with a long lifetime.
-///
+/** ---------------------------------------------------------------------------
+ * A class to parse a command string. This must be treated as a temporary
+ * object and should not be stored in an object with a long lifetime.
+ */
 class ArgString {
 
 	// the command string given
@@ -39,35 +39,35 @@ class ArgString {
 
 public:
 
-	/// -----------------------------------------------------------------------
-	/// Parse an argument string.
-	///
-	/// @param input Line of text to parse into arguments.
-	///
+	/** -----------------------------------------------------------------------
+	 *  Parse an argument string.
+	 * 
+	 *  @param input Line of text to parse into arguments.
+	 */
 	ArgString( const Util::StringRef &input );
 
-	/// -----------------------------------------------------------------------
-	/// Checks that the arg string contains the desired input.
-	///
-	/// @param usage       List of expected arguments.
-	/// @param start       Offset of argument list. Usually 1 to skip the 
-	///                    command name.
-	/// @param allow_extra If false, fail if there are more arguments present
-	///                    than desired. If true, ignore any additional
-	///                    arguments.
-	///
-	/// @returns true if the arguments are present and can convert into the
-	///          desired types.
-	///
-	/// For example Check( {STRING,STRING,INTEGER} ) will check if there are
-	/// at least 3 arguments (plus the command), and checks if the third 
-	/// command argument is a valid integer.
-	///
-	/// STRING checks for any argument.
-	/// INT checks for a valid integer.
-	/// LONG checks for a valid 64-bit integer.
-	/// FLOAT checks for a valid decimal number.
-	///
+	/** -----------------------------------------------------------------------
+	 *  Checks that the arg string contains the desired input.
+	 * 
+	 *  @param usage       List of expected arguments.
+	 *  @param start       Offset of argument list. Usually 1 to skip the 
+	 *                     command name.
+	 *  @param allow_extra If false, fail if there are more arguments present
+	 *                     than desired. If true, ignore any additional
+	 *                     arguments.
+	 * 
+	 *  @returns true if the arguments are present and can convert into the
+	 *           desired types.
+	 * 
+	 *  For example Check( {STRING,STRING,INTEGER} ) will check if there are
+	 *  at least 3 arguments (plus the command), and checks if the third 
+	 *  command argument is a valid integer.
+	 * 
+	 *  STRING checks for any argument.
+	 *  INT checks for a valid integer.
+	 *  LONG checks for a valid 64-bit integer.
+	 *  FLOAT checks for a valid decimal number.
+	 */
 	bool Check( std::initializer_list<int> usage, int start = 1, 
 				bool allow_extra = true );
 	
@@ -78,23 +78,23 @@ public:
 		FLOAT
 	};
 
-	/// -----------------------------------------------------------------------
-	/// @returns number of arguments parsed.
-	///
+	/** -----------------------------------------------------------------------
+	 *  @returns number of arguments parsed.
+	 */
 	int Count() const { return (int)m_index.size(); }
 
 	// Note: All Get functions return an "empty" value if the
 	// index is out of range.
 
-	/// -----------------------------------------------------------------------
-	/// Get the full command string starting from the specified index.
-	///
-	/// @param start Starting index to read from.
-	/// @returns Copy of argument.
-	///
-	/// For example, with the input "aaa  bbb     cccc", 
-	/// GetFull(1) will return "bbb     cccc"
-	///
+	/** -----------------------------------------------------------------------
+	 *  Get the full command string starting from the specified index.
+	 * 
+	 *  @param start Starting index to read from.
+	 *  @returns Copy of argument.
+	 * 
+	 *  For example, with the input "aaa  bbb     cccc", 
+	 *  GetFull(1) will return "bbb     cccc"
+	 */
 	std::string GetFull( int start ) const {
 		if( start < 0 || start >= (int)m_index.size() ) return "";
 		
@@ -105,37 +105,37 @@ public:
 			.substr( first.start, last.start+last.length-first.start );
 	}
 
-	/// -----------------------------------------------------------------------
-	/// Read an argument as a string.
-	/// 
-	/// @param index Argument index.
-	/// @returns Copy of argument.
-	///
+	/** -----------------------------------------------------------------------
+	 *  Read an argument as a string.
+	 *  
+	 *  @param index Argument index.
+	 *  @returns Copy of argument.
+	 */
 	std::string GetString( int index ) const {
 		if( index < 0 || index >= (int)m_index.size() ) return "";
 		return m_source
 			.substr( m_index[index].start, m_index[index].length );
 	}
 	
-	/// -----------------------------------------------------------------------
-	/// Read an argument into the given buffer.
-	/// 
-	/// @param index  Argument index.
-	/// @param output Buffer to copy to.
-	/// @param maxlen Size of buffer.
-	///
+	/** -----------------------------------------------------------------------
+	 *  Read an argument into the given buffer.
+	 *  
+	 *  @param index  Argument index.
+	 *  @param output Buffer to copy to.
+	 *  @param maxlen Size of buffer.
+	 */
 	void GetCString( int index, char *output, int maxlen ) const;
 
-	/// -----------------------------------------------------------------------
-	/// Get an argument as pointers to the internal buffer.
-	///
-	/// @param index  Index of argument.
-	/// @param start  (Output) String address.
-	/// @param length (Output) Length of string returned.
-	///
-	/// If the index is out of range, *start will be set to nullptr and length
-	/// will be set to 0.
-	///
+	/** -----------------------------------------------------------------------
+	 *  Get an argument as pointers to the internal buffer.
+	 * 
+	 *  @param index  Index of argument.
+	 *  @param start  (Output) String address.
+	 *  @param length (Output) Length of string returned.
+	 * 
+	 *  If the index is out of range, *start will be set to nullptr and length
+	 *  will be set to 0.
+	 */
 	void GetStrptr( int index, const char *&start, int &length ) {
 		
 		if( index < 0 || index >= (int)m_index.size() ) {
@@ -148,37 +148,37 @@ public:
 		length = m_index[index].length;
 	}
 
-	/// -----------------------------------------------------------------------
-	/// Read an integer value.
-	///
-	/// @param index Index of argument.
-	/// @param easy  Retrun 0 if no conversion can be performed instead of
-	///              throwing an exception.
-	/// @param base  Base to use for conversion. If 0, the base will be
-	//               detected. (see std::stoi documentation)
-	///
-	/// @throws BadConversion if the input cannot be converted to an integer.
-	/// 
+	/** -----------------------------------------------------------------------
+	 *  Read an integer value.
+	 * 
+	 *  @param index Index of argument.
+	 *  @param easy  Retrun 0 if no conversion can be performed instead of
+	 *               throwing an exception.
+	 *  @param base  Base to use for conversion. If 0, the base will be
+	 *               detected. (see std::stoi documentation)
+	 * 
+	 *  @throws BadConversion if the input cannot be converted to an integer.
+	 */
 	int GetInt( int index, bool easy = true, int base = 0 ) const;
 	long long GetLongInt( int index, bool easy = true, int base = 0 ) const;
 	
-	/// -----------------------------------------------------------------------
-	/// Read a floating point value.
-	///
-	/// @param index Index of argument.
-	/// @param easy  If true, 0.0 is returned on failure instead of throwing
-	///              an exception.
-	///
-	/// @throws BadConversion if the input cannot be converted.
-	/// 
+	/** -----------------------------------------------------------------------
+	 *  Read a floating point value.
+	 * 
+	 *  @param index Index of argument.
+	 *  @param easy  If true, 0.0 is returned on failure instead of throwing
+	 *               an exception.
+	 * 
+	 *  @throws BadConversion if the input cannot be converted.
+	 */
 	double GetFloat( int index, bool easy = true ) const;
 
-	/// -----------------------------------------------------------------------
-	/// Read an argument
-	///
-	/// @param index Index of argument.
-	/// @returns argument, or "" if the index is out of bounds.
-	///
+	/** -----------------------------------------------------------------------
+	 *  Read an argument
+	 * 
+	 *  @param index Index of argument.
+	 *  @returns argument, or "" if the index is out of bounds.
+	 */
 	std::string operator[]( int index ) const {
 		return GetString( index );
 	}
