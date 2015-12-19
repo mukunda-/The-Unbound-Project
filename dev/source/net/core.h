141589f6789abe5ea6aa6f67338ef27189fab7b9
mukunda- <mukunda@mukunda.com>
2015-12-19 17:21:13 -0600

    a

--- dev/source/net/core.h
+++ dev/source/net/core.h
@@ -1,6 +1,6 @@
 //==========================  The Unbound Project  ==========================//
 //                                                                           //
-//========= Copyright © 2014, Mukunda Johnson, All rights reserved. =========//
+//========= Copyright © 2015, Mukunda Johnson, All rights reserved. =========//
 
 #pragma once
 
@@ -14,30 +14,30 @@ namespace Net {
 	System::Service &DefaultService(); 
 	void Run( int threads ); 
 
-	/// -----------------------------------------------------------------------
-	/// Make a remote connection.
-	///
-	/// This is a blocking function that connects to the specified address.
-	///
-	/// @param host     Address to connect to
-	/// @param service  Service/port number to connect to.
-	/// @param factory  Factory to create the stream with.
-	/// 
-	/// @returns New stream pointer.
-	///
-	/// @throws boost::system::system_error on failure.
-	///
+	/** -----------------------------------------------------------------------
+	 * Make a remote connection.
+	 *
+	 * This is a blocking function that connects to the specified address.
+	 *
+	 *  @param host     Address to connect to
+	 *  @param service  Service/port number to connect to.
+	 *  @param factory  Factory to create the stream with.
+	 *  
+	 *  @returns New stream pointer.
+	 * 
+	 *  @throws boost::system::system_error on failure.
+	 */ 
 	Stream::ptr Connect( const std::string &host, 
 						 const std::string &service,
 						 StreamFactory factory );
 	
-	/// -----------------------------------------------------------------------
-	/// Make a remote connection asynchronously.
-	///
-	/// @param host    Remote address.
-	/// @param service Service/port number to connect to. 
-	/// @param factory Factory to create the stream with.
-	///
+	/** -----------------------------------------------------------------------
+	 *  Make a remote connection asynchronously.
+	 * 
+	 *  @param host    Remote address.
+	 *  @param service Service/port number to connect to. 
+	 *  @param factory Factory to create the stream with.
+	 */ 
 	void ConnectAsync( const std::string &host, 
 					   const std::string &service,  
 					   StreamFactory factory );
